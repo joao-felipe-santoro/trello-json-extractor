aeebe0b65b33bf5a99dd005e6d772b6423b8b41d
joao Felipe Santoro Pinto <joao.santoro.pinto@concrete.com.br>
2020-08-12 23:28:59 -0300

    feat: Command line interaction and save to csv

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 const fs = require('fs');
+const commander = require('commander');
+const converter = require('json-2-csv');
+
 
 const { getTrelloData } = require('./trelloExtractor');
 const { joinSchedulingData } = require('./joinSchedulingData');
@@ -11,12 +14,30 @@ const schedulingJsonfile = process.env.SCHEDULING_JSON_FILE;
 const apiKey = process.env.API_KEY;
 const apiToken = process.env.API_TOKEN;
 
+commander
+    .version('1.0.0', '-v, --version')
+    .usage('[OPTIONS]...')
+    .option('-s --sync', 'Sync data with Trello')
+    .option('-x --extract', 'Save Trello data to file')
+    .option('-j --join', 'Save Trello with Scheduling data to file')
+    .option('-c --csv', 'Generate CSV file')
+    .parse(process.argv);
+
 (async () => {
     const chapterData = await getTrelloData(trelloJsonfile);
     const jointData = await joinSchedulingData(chapterData, schedulingJsonfile);
-    await updateTrello(apiKey, apiToken, jointData);
-    await writeData(process.env.CHAPTER_JSON_FILE, chapterData) ;
-    await writeData(process.env.JOINT_JSON_FILE, jointData) ;
+    if (commander.sync) {
+        await updateTrello(apiKey, apiToken, jointData);
+    }
+    if (commander.extract) {
+        await writeData(process.env.CHAPTER_JSON_FILE, chapterData);
+    }
+    if (commander.join) {
+        await writeData(process.env.JOINT_JSON_FILE, jointData);
+    }
+    if(commander.csv) {
+        await writeCsv(jointData);
+    }
 
 })();
 
@@ -27,3 +48,17 @@ async function writeData(file, data) {
         console.log("Successfully Written to File.");
     });
 };
+
+async function writeCsv(data) {
+    converter.json2csv(data, (err, csv) => {
+        if (err) {
+            throw err;
+        }
+
+        fs.writeFile('data/data.csv', csv, (err) => {
+            if (err)
+                console.log(err);
+            console.log("Successfully Written to File.");
+        });
+    });
+}
