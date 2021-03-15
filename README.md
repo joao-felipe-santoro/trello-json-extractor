355fe0844e089194226a3abfed88472634ac3700
joao Felipe Santoro Pinto <joao.santoro.pinto@concrete.com.br>
2021-03-15 15:49:49 -0300

    Removing functionalities no longer used

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 trello-json-extractor
+
+# Options Available 
+| Option        | Action                   |
+| ------------- | -------------------------|
+| - x --extract | Save Trello Date to File |
+| - c --csv     | Generate CSV File        |
--- main.js
+++ main.js
@@ -7,7 +7,6 @@ const converter = require('json-2-csv');
 const { getTrelloData } = require('./trelloExtractor');
 const { joinSchedulingData } = require('./joinSchedulingData');
 const { joinRosterData } = require('./joinRosterData');
-const { updateTrello } = require('./updateTrello');
 
 const schedulingJsonfile = process.env.SCHEDULING_IN_FILE;
 const rosterJsonfile = process.env.ROSTER_IN_FILE;
@@ -19,33 +18,17 @@ const boardId = process.env.BOARD_ID;
 commander
     .version('1.0.0', '-v, --version')
     .usage('[OPTIONS]...')
-    .option('-s --sync', 'Sync data with Trello')
     .option('-x --extract', 'Save Trello data to file')
-    .option('-js --joinSch', 'Save Trello with Scheduling data to file')
-    .option('-jr --joinRoster', 'Save Trello with Roster data to file')
     .option('-c --csv', 'Generate CSV file')
     .parse(process.argv);
 
 (async () => {
     const trelloData = await getTrelloData(apiKey, apiToken, boardId);
-    const schedulingData = await joinSchedulingData(trelloData, schedulingJsonfile);
-    const rosterData = await joinRosterData(schedulingData, rosterJsonfile);
     if (commander.extract) {
         await writeData(process.env.TRELLO_OUT_FILE, trelloData);
     }
-    if (commander.joinSch) {
-
-        await writeData(process.env.SCHEDULING_OUT_FILE, schedulingData);
-    }
-    if (commander.joinRoster) {
-
-        await writeData(process.env.ROSTER_OUT_FILE, rosterData);
-    }
     if (commander.csv) {
-        await writeCsv(rosterData);
-    }
-    if (commander.sync) {
-        await updateTrello(apiKey, apiToken, trelloData, rosterData);
+        await writeCsv(trelloData);
     }
 })();
 
@@ -58,14 +41,7 @@ async function writeData(file, data) {
 };
 
 async function writeCsv(data) {
-    csvData = data.map((entry) => {
-        entry.bookedUntil = entry.bookedUntil.replace(/(T\d\d:\d\d:\d\d\.\d\d\dZ)/, '');
-        entry.hireDate = entry.hireDate.replace(/(T\d\d:\d\d:\d\d\.\d\d\dZ)/, '');
-        entry.lastPromo = entry.lastPromo.replace(/(T\d\d:\d\d:\d\d\.\d\d\dZ)/, '');
-        entry.birthday = entry.birthday.replace(/(T\d\d:\d\d:\d\d\.\d\d\dZ)/, '');
-        return entry;
-    });
-    converter.json2csv(csvData, (err, csv) => {
+    converter.json2csv(data, (err, csv) => {
         if (err) {
             throw err;
         }
